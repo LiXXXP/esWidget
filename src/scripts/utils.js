27c42d4e02b2f03a2951358ef71c07c300868283
lxp <lixueping906@gmail.com>
2021-01-07 12:11:43 +0800

    时间

--- src/scripts/utils.js
+++ src/scripts/utils.js
@@ -381,7 +381,7 @@ function UTCDateToLocalDate(dateStr) {
     var offsetMinute = date1.getTimezoneOffset()
     var offsetHours = offsetMinute / 60
     var date2 = new Date(dateStr)
-    date2.setHours(date2.getHours() - offsetHours)
+    // date2.setHours(date2.getHours() - offsetHours)
     return date2.toLocaleString()
 }
 
